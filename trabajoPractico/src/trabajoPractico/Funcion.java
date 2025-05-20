85380777c11d42db1d80128a1930f0eb9442950a
Diego Arias <diegoalexd04@gmail.com>
2025-05-19 21:07:10 -0300

    insteadOf

--- trabajoPractico/src/trabajoPractico/Funcion.java
+++ trabajoPractico/src/trabajoPractico/Funcion.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 
 public class Funcion {
 	private Sede sede;
+	private SedeConSectores sedeConSectores;
 	private LocalDate fecha;
 	private double precioBase;
 	private HashMap<String, boolean[]> asientos;
@@ -12,11 +13,25 @@ public class Funcion {
 	
 	Funcion(Sede sede, String fecha, double precioBase){
 		this.sede = sede;
-		
 		this.precioBase = precioBase;
 		this.asientos = new HashMap<String, boolean[]>();
 		this.entradasVendidas = new HashMap<String, Entrada>();
 		
-		
+		if(sede instanceof SedeConSectores) {
+			SedeConSectores sedeConSec = (SedeConSectores) sede; 
+			
+			String[] sectoresDeSede = sedeConSec.sectores();
+			for(int i = 0; i < sectoresDeSede.length; i++) {
+				
+				int capacidadPorSector = sedeConSec.capacidadPorSector().length;
+				boolean[] asientosBoolean = new boolean[capacidadPorSector];
+				
+				for(int x = 0; x<capacidadPorSector; x++) {
+					asientosBoolean[x] = true;
+				}
+				
+				asientos.put(sectoresDeSede[i], asientosBoolean);
+			}
+		}
 	}
 }
--- trabajoPractico/src/trabajoPractico/SedeConSectores.java
+++ trabajoPractico/src/trabajoPractico/SedeConSectores.java
@@ -2,8 +2,8 @@ package trabajoPractico;
 
 import java.util.HashMap;
 
-public class SedeConSectores extends Sede {
-	private HashMap<String , Sector> sectores;
+public abstract class SedeConSectores extends Sede {
+	private HashMap<String, Sector> sectores;
 	
 	SedeConSectores(String nombre, String direccion, int capacidadMaxima, int asientosPorFila, String[] sectores, int[] capacidad, int[] porcentajeAdicional) {
 		super(nombre, direccion, capacidadMaxima);
@@ -25,5 +25,15 @@ public class SedeConSectores extends Sede {
 		}
 		
 		return sectores;
+	}
+	
+	public int[] capacidadPorSector() {
+		int[] capacidad = new int[this.sectores.size()];
+		int i = 0;
+		for(HashMap.Entry<String, Sector> entry : this.sectores.entrySet()) {
+			capacidad[i] = entry.getValue().capacidad();
+			i++;
+		}
+		return capacidad;
 	}
 }
